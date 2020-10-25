eb23bc39de2133a4f103d89b97223c8a1b0324ff
Vasilis <vatsakiris@gmail.com>
2020-10-25 23:32:35 +0000

    Setting up the start skeleton.

---
+++ ticket-configuration/src/main/java/gr/bill/dissertation/ticket/TicketApplication.java
@@ -0,0 +1,15 @@
+package gr.bill.dissertation.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketApplication.class, args);
+    }
+
+
+}
+
