8eebd6e8e365cfb923df8d5697eeef29132ca274
Vasilis <vatsakiris@gmail.com>
2020-10-28 18:05:21 +0000

    Create package structure that follows hexagonal architecture.

--- ticket-configuration/src/main/java/gr/bill/dissertation/ticket/TicketApplication.java
+++ ticket-configuration/src/main/java/gr/bill/dissertation/ticket/TicketApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketApplication {
+public class TicketApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketApplication.class, args);
     }
 
-
 }
-
